0c86c40484de398119e73ce5e5d406237623c2e9
spleeter <ngdnam03@gmail.com>
2025-08-06 17:11:35 +0700

    add enums

---
+++ src/constants/enums.js
@@ -0,0 +1,25 @@
+// 1. Export Status (backend trả name, backend nhận code số)
+export const ExportStatusValueMap = {
+    NOT_EXPORTED: 0,
+    PARTIALLY_EXPORTED: 1,
+    FULLY_EXPORTED: 2,
+};
+
+// 2. Product Type (backend trả name, backend nhận value chuỗi)
+export const ProductTypeValueMap = {
+    AIRCON: 'Aircon',
+    SPARE_PART: 'Spare_part',
+};
+
+// 3. Supplier Code (backend trả name, backend nhận value chuỗi)
+export const SupplierCdValueMap = {
+    VIET_NAM: 'VN',
+    THAILAND: 'TH',
+    MALAYSIA: 'MY',
+    INDONESIA: 'ID',
+    SINGAPORE: 'SG',
+    PHILIPPINES: 'PH',
+    LAOS: 'LA',
+    MYANMAR: 'MM',
+    DONGTIMOR: 'TL',
+};
